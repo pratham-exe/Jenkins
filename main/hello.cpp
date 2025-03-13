c2faf2bf5d32a5ffaec37f9b9d41d0b9782400af
pratham-exe <prathamrao001@gmail.com>
2025-03-13 18:46:24 +0530

    Task 4

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is CC Lab 8\n";
+  cout << "This is Task 4\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
