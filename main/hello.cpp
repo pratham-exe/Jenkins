6060c4bde2524c73a05587b1709b549ba739cb12
pratham-exe <prathamrao001@gmail.com>
2025-03-13 17:12:41 +0530

    Added one line

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "This is CC Lab 8\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
